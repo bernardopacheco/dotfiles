42121c08603665a42e8c63518b7e708f36b21cde
Bernardo Pacheco <bernardo.wpacheco@gmail.com>
2014-11-12 12:31:20 -0200

    fix(atom): add monokai theme in the packages file to be installed

--- install.fish
+++ install.fish
@@ -1,3 +1,9 @@
 # Atom editor
+echo \n
 ln -s ~/src/dotfiles/.atom ~/.atom
-echo "atom edtior symlink created"
+echo "atom editor: symlink created"
+echo \n
+
+apm install --packages-file .atom/packages.txt
+echo "atom editor: packages installed"
+echo \n
