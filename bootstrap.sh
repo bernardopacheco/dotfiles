0719ea283f08a17d5919c8525e144f75aaab876b
Bernardo Pacheco <bernardo.wpacheco@gmail.com>
2014-11-17 11:36:34 -0200

    refactor(bootstrap): change configuring messages

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,12 +8,17 @@ function ensure_link {
     test -L "$HOME/$2" || ln -s "$HOME/$1" "$HOME/$2"
 }
 
-# Atom editor
-echo
-ensure_link "src/dotfiles/.atom" ".atom"
-echo "atom editor: symlink created"
 echo
 
+# Atom editor
+echo ATOM EDITOR
+echo
+echo "configuring symlink..."
+ensure_link "src/dotfiles/.atom" ".atom"
+echo "done"
+echo
+
+echo "installing packages..."
 apm install --packages-file .atom/packages.txt
-echo "atom editor: packages installed"
+echo "done"
 echo
