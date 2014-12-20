d81d726160e91edb060d8a3b821b3156728a28c1
Bernardo Pacheco <bernardo.wpacheco@gmail.com>
2014-12-20 15:37:32 -0200

    feat(vim): add pathogen

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,6 +13,9 @@ mkdir -p ~/local/bin
 cp bin/* ~/local/bin
 chmod 744 ~/local/bin/*
 
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+test -e ~/.vim/autoload/pathogen.vim || curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
 ensure_link "src/dotfiles/vim/.vimrc" ".vimrc"
 ensure_link "src/dotfiles/fish/config.fish" ".config/fish/config.fish"
 ensure_link "src/dotfiles/git/.gitconfig" ".gitconfig"
---
+++ vim/.vimrc
@@ -0,0 +1,9 @@
+" .vimrc
+" Author: Bernardo Pacheco <bernardo@bernardopacheco.net>
+
+" Configure pathogen
+" https://github.com/tpope/vim-pathogen
+
+execute pathogen#infect()
+syntax on
+filetype plugin indent on
